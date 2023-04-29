03716d2338628d23e93bccb0b1624fa0212b9703
CICADA3310 <36794918+git-chinmay@users.noreply.github.com>
2023-04-29 08:46:16 +0530

    loading plugin and running tasks for file concatination

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,19 +3,37 @@ module.exports = function(grunt){
     //Configuration
     grunt.initConfig({
         //pass in options, plugins , refrences to files ..etc
+        concat:{
+            js: {
+                //src: ['js/jquery-1.12.4.js', 'js/rslides.js', 'js/scripts.js']
+                src: ['js/*.js'],
+                dest: 'build/scripts.js'
+            },
+            css: {
+                src: ['CSS/*.css'],
+                dest: 'build/styles.js'
+            }
+        }
     });
 
     //Load plugins
-    //grunt.loadNpmTasks('<plugin name>')
+    grunt.loadNpmTasks('grunt-contrib-concat')
 
-    //Rgister tasks needs to run
-    grunt.registerTask('run', ()=>{
-        console.log("Running task 'RUN'.");
-    })
+    //REGISTER TASKS NEED TO RUN
+    //Examples
+    // grunt.registerTask('run', ()=>{
+    //     console.log("Running task 'RUN'.");
+    // })
 
-    grunt.registerTask('sleep', ()=>{
-        console.log("Running task 'SLEEP'.");
-    })
+    // grunt.registerTask('sleep', ()=>{
+    //     console.log("Running task 'SLEEP'.");
+    // })
 
-    grunt.registerTask('all', ['run', 'sleep'])
+    // grunt.registerTask('all', ['run', 'sleep'])
+    
+    //Actual Tasks
+    grunt.registerTask('concat-js', ['concat:js'])
+
+    grunt.registerTask('concat-css', ['concat:css'])
+
 }
